e3873432e846e89d83c45042e53c6c65413b1909
SideralSkylark <sidikebrahimserage@gmail.com>
2025-04-09 16:14:55 +0200

    added auth guards to routes that require authentication

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent},
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: '**', redirectTo: '/login' },
     // nao ponhas rotas abaixo desta utlima, poe antes dela.
 ];
