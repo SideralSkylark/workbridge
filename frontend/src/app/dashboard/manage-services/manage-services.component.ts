2bcbffc5c9bce2f597aa452d90d0bdae436bc64b
SideralSkylark <sidikebrahimserage@gmail.com>
2025-05-17 21:29:46 +0200

    added functionality to open billbridge from workbridge

--- frontend/src/app/dashboard/manage-services/manage-services.component.ts
+++ frontend/src/app/dashboard/manage-services/manage-services.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { ServiceFormStateService } from '../../services/service-form-state.service';
 import { filter } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-manage-services',
@@ -16,7 +17,8 @@ export class ManageServicesComponent {
 
   constructor(
     private router: Router,
-    private formStateService: ServiceFormStateService
+    private formStateService: ServiceFormStateService,
+    private http: HttpClient  
   ) {
     this.currentRoute = this.router.url;
 
@@ -39,6 +41,33 @@ export class ManageServicesComponent {
 
   viewBookings(): void {
     this.router.navigate(['dashboard', 'manage', 'bookings']);
+  }
+
+  openBillBridge(): void {
+    const token = localStorage.getItem('jwt');
+    if (!token) {
+      console.error('Token JWT não encontrado.');
+      return;
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    this.http.get('http://localhost:8180/auth', { headers, responseType: 'text' })
+      .subscribe({
+        next: (html) => {
+          const newWindow = window.open('', '_blank');
+          if (newWindow) {
+            newWindow.document.open();
+            newWindow.document.write(html);
+            newWindow.document.close();
+          } else {
+            console.error('Falha ao abrir nova aba.');
+          }
+        },
+        error: (err) => {
+          console.error('Erro ao acessar BillBridge:', err);
+        }
+      });
   }
 
   isOnServicesPage(): boolean {
